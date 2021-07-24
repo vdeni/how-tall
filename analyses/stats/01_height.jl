6073db0475587ac0db5367991e4b69947b968a45
Denis Vlašiček <denis.vlasicek@gmail.com>
2021-07-24 16:46:54 +0200

    add density of heights and prior

--- analyses/stats/01_height.jl
+++ analyses/stats/01_height.jl
@@ -18,47 +18,76 @@ d = CSV.read(joinpath("analyses",
              DataFrames.DataFrame)
 
 # plot height by day of week
-d.weekday = CategoricalArrays.CategoricalArray(Dates.dayname.(d.date),
+d.weekday = CategoricalArrays.CategoricalArray(Dates.dayname.(d.date);
                                                ordered = true)
 
 CategoricalArrays.levels!(d.weekday,
                           ["Monday", "Tuesday", "Wednesday", "Thursday",
                            "Friday", "Saturday", "Sunday"])
 
+# set plotting variables
 marker_color = Plots.palette(:viridis, 3)[2]
+line_color_1 = Plots.palette(:viridis, 3)[1]
+line_color_2 = Plots.palette(:viridis, 3)[2]
 
-p_height_by_day = @df d scatter(:weekday,
-                                :height_cm;
-                                yticks = 194:200,
-                                ylims = (194.5, 199.5),
-                                xlims = (0.4, 7.5),
-                                xticks = 1:7,
-                                legend = false,
-                                color = marker_color,
-                                dpi = 300,
-                                xlabel = "Day of the week",
-                                ylabel = "Height in centimeters",
-                                markeralpha = .7,
-                                markerstrokewidth = 1,
-                                gridstyle = :dash)
+# make plots
+p_height_by_day = @df d Plots.scatter(:weekday,
+                                      :height_cm;
+                                      yticks = 194:200,
+                                      ylims = (194.5, 199.5),
+                                      xlims = (0.4, 7.5),
+                                      xticks = 1:7,
+                                      legend = false,
+                                      color = marker_color,
+                                      dpi = 300,
+                                      xlabel = "Day of the week",
+                                      ylabel = "Height in centimeters",
+                                      markeralpha = .7,
+                                      markerstrokewidth = 1,
+                                      gridstyle = :dash)
+
+# Plots.plot(p_height_by_day,
+#            size = (1920, 1080),
+#            margin = 10Plots.mm) |>
+#     x -> Plots.savefig(x,
+#                        joinpath("analyses",
+#                                 "stats",
+#                                 "p_height_by_day.pdf"))
 
 # plot height by time of day
-d_p_by_hour = DataFrames.filter( x -> x.time_hours > 0, d)
+d_p_by_hour = DataFrames.filter(x -> x.time_hours > 0,
+                                d)
 
-p_height_by_hour = @df d_p_by_hour scatter(:time_hours,
-                                           :height_cm;
-                                           yticks = 194:199,
-                                           ylims = (194.5, 199.5),
-                                           xticks = 10:2:20,
-                                           xlims = (9, 21),
-                                           legend = false,
-                                           color = marker_color,
-                                           dpi = 300,
-                                           xlabel = "Time of day (hours)",
-                                           ylabel = "Height in centimeters",
-                                           markeralpha = .7,
-                                           markerstrokewidth = 1,
-                                           gridstyle = :dash)
+p_height_by_hour = @df d_p_by_hour Plots.scatter(:time_hours,
+                                                 :height_cm;
+                                                 yticks = 194:199,
+                                                 ylims = (194.5, 199.5),
+                                                 xticks = 10:2:20,
+                                                 xlims = (9, 21),
+                                                 legend = false,
+                                                 color = marker_color,
+                                                 dpi = 300,
+                                                 xlabel = "Time of day (hours)",
+                                                 ylabel = "Height in centimeters",
+                                                 markeralpha = .7,
+                                                 markerstrokewidth = 1,
+                                                 gridstyle = :dash)
+
+# plot height distribution and prior for height
+p_height_distr = @df d Plots.density(:height_cm;
+                                     dpi = 300,
+                                     color = line_color_1,
+                                     yaxis = nothing,
+                                     xlabel = "Height in centimeters",
+                                     grid = false,
+                                     yshowaxis = false,
+                                     label = "Data")
+
+Plots.plot!(p_height_distr,
+            Turing.Normal(196, .75),
+            legend = :topright,
+            label = "Prior",
+            color = line_color_2)
 
 # build height estimation model
 # my initial belief is that I'm 196 cm tall. I'd consider anything between 194
